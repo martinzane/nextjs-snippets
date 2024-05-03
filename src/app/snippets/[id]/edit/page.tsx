66b1cc8556cfa21742a8528ecbf9a558cc1dd4ce
Martin Zane <martin@zaneweb.com>
2024-05-03 15:55:15 +0200

    Add Edit snippet page

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,21 @@
+import db from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetEditPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const id = parseInt(props.params.id);
+  const snippet = await db.snippet.findFirst({
+    where: { id },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>Editing snippet &quot;{snippet.title}&quot;</div>;
+}
