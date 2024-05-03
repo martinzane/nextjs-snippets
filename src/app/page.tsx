1d0bcaea00a0b27f8e69ced4590eda432a22ff53
Martin Zane <martin@zaneweb.com>
2024-05-03 15:45:35 +0200

    Add basic styling for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import db from "@/db";
+import Link from "next/link";
 
 export default async function HomePage() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => (
-    <div key={snippet.id}>{snippet.title}</div>
+    <Link
+      key={snippet.id}
+      className="flex justify-between items-center p-2 border rounded"
+      href={`/snippets/${snippet.id}`}
+    >
+      <div>{snippet.title}</div>
+      <div>View</div>
+    </Link>
   ));
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center my-2">
+        <h1 className="text-xl bold">Snippet</h1>
+        <Link href="/snippets/create" className="border p-2 border rounded">New</Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
