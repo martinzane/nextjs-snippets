2cefbab405e02ad78aeb45790e1107aa11a93b8b
Martin Zane <martin@zaneweb.com>
2024-05-03 15:20:54 +0200

    Fetch snippets on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function HomePage() {
-  return <div>Home page</div>;
+import db from "@/db";
+
+export default async function HomePage() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => (
+    <div key={snippet.id}>{snippet.title}</div>
+  ));
+
+  return <div>{renderedSnippets}</div>;
 }
